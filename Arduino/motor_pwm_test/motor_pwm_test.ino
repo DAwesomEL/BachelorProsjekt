372f6d9c3da02a488a58b0e11fdc071b3c69ebdf
Tocuu <62709325+Tocuu@users.noreply.github.com>
2023-03-16 17:22:10 +0100

    Arbeidstegning og arduino kode

--- Arduino/motor_pwm_test/motor_pwm_test.ino
+++ Arduino/motor_pwm_test/motor_pwm_test.ino
@@ -1,35 +1,39 @@
-int motor = 9;
-int motor2 = 10;
+int PWM = ;
 int INA = 7;
 int INB = 8;
+int EN = A0;
 int speed = 0; 
 int pwmIncrement = 5; 
-int testDir = 2;
-
-
-
 
 
 void setup() {
 
-   pinMode(motor, OUTPUT);
-   pinMode(motor2, OUTPUT);
-   pinMode(testDir, OUTPUT);
+  Serial.begin(9600);
+  pinMode(PWM,OUTPUT);
+  pinMode(INA,OUTPUT);
+  pinMode(INB,OUTPUT);
+  pinMode(EN,OUTPUT);
+
 }
-
-
 
 void loop() {
 
-   analogWrite(motor, 255);
+  digitalWrite(INA,HIGH); 
+  digitalWrite(INB,LOW);
+  digitalWrite(EN,HIGH);
+
+  analogWrite(PWM, speed);
    
-   speed = speed + pwmIncrement;
+  speed = speed + pwmIncrement;
 
    if (speed == 0 || speed == 255) {
       pwmIncrement = -pwmIncrement ;
    }
+  
+  Serial.print(speed);
 
-    digitalWrite(testDir,HIGH);
+  
 
-   delay(300);
+  delay(300);
+
 }
